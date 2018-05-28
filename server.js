9553a8044501679faf744d01ecdcf25c54a000a4
VarmaSANJAY <sanjay.varma@relevacelab.com>
2018-05-28 12:36:53 +0530

    Signup part 3

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var User = require('./models/user');
 var bodyParser = require('body-parser');
 var ejs = require('ejs');
 var ejsMate = require('ejs-mate');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var flash = require('express-flash');
 var app = express();
 
 mongoose.connect('mongodb://localhost:27017/ecommerce', function (err) {
@@ -21,6 +24,13 @@ app.use(morgan('dev'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({encoded: true}));
+app.use(cookieParser());
+app.use(session({
+    resave: true,  //Forces the session to save back session store
+    saveUninitialized: true, //Forces the session which is uninitizalied to save , a session is uninitizalied when it is new
+    secret:"Pass@123"
+}));
+app.use(flash());
 app.engine('ejs',ejsMate);
 app.set('view engine', 'ejs');
 
@@ -29,7 +39,7 @@ var userRoutes = require('./routes/user');
 app.use(mainRoutes);
 app.use(userRoutes);
 
-app.listen(3000, function (err) {
+app.listen(3004, function (err) {
     if(err) throw err;
     console.log("Server is running");
 });
