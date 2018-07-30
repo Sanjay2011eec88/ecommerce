1fce7f647823bbbeadaa667bcef558e56e9f8df9
VarmaSANJAY <sanjay.varma@relevacelab.com>
2018-07-30 12:13:59 +0530

    Added cart routes

--- routes/main.js
+++ routes/main.js
@@ -47,6 +47,19 @@ stream.on('error',function(err){
     console.log(err);
 });
 
+router.get('/cart', function(req, res, next) {
+    Cart
+        .findOne({ owner: req.user._id })
+        .populate('items.item')
+        .exec(function(err, foundCart) {
+            if (err) return next(err);
+            res.render('main/cart', {
+                foundCart: foundCart,
+                message: req.flash('remove')
+            });
+        });
+});
+
 router.post('/product/:product_id', function(req, res, next) {
     Cart.findOne({ owner: req.user._id }, function(err, cart) {
         cart.items.push({
